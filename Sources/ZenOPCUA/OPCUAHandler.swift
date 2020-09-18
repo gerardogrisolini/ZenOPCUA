7df910ffe63ae7dea1d0b978342c1b8d03ae9d7c
Gerardo Grisolini <gerardo.grisolini@gmail.com>
2020-09-18 15:52:55 +0200

    Refactoring

--- Sources/ZenOPCUA/OPCUAHandler.swift
+++ Sources/ZenOPCUA/OPCUAHandler.swift
@@ -147,7 +147,7 @@ final class OPCUAHandler: ChannelInboundHandler, RemovableChannelHandler {
             case .serviceFault:
                 let part = frame.body[20...43].map { $0 }
                 let responseHeader = ResponseHeader(bytes: part)
-                let error = OPCUAError.generic("serviceFault")
+                let error = OPCUAError.generic("\(responseHeader.serviceResult)")
                 promises[responseHeader.requestHandle]?.fail(error)
                 onErrorCaught(context: context, error: error)
             default:
